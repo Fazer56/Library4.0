85eae3f9eb2ba9a5cc417492bda0c41d16f7afb2
Fazer56 <faoilean56@gmail.com>
2018-05-07 16:11:50 +0200

    First Commit

---
+++ Library2.0/Library/Controllers/BookController.cs
@@ -0,0 +1,48 @@
+﻿using Library.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Library.Controllers
+{
+    public class BookController : Controller
+    {
+		// GET: Book
+		public ActionResult BookList()
+		{
+			LibraryEntities db = new LibraryEntities();
+			var books = from Aut in db.Authors
+						join It in db.Items
+						on Aut.AuthorID equals It.AuthorID
+						select new ItemLibrarianViewModel { It = It, Aut = Aut};
+
+			foreach (ItemLibrarianViewModel i in books.ToList())
+			{
+				Debug.WriteLine("YOOOOOOOOO");
+				Copy cp = new Copy();
+				for (int z = 0; z < 5; z++)
+				{
+					cp.Isbn = i.It.Isbn;
+					cp.Borrowed = "n";
+					db.Copies.Add(cp);
+				}
+				db.SaveChanges();
+
+			}
+
+			books.ToList();
+			/*var books = from b in db.Items
+						join a in db.Authors
+						on b.AuthorID equals a.AuthorID
+						select new { a.AuthName, b.Isbn, b.Name, b.Subject, b.Year };
+			books.ToList();
+			ViewData["booklist"] = books;*/
+			
+
+			return View(books);
+        }
+    }
+}
